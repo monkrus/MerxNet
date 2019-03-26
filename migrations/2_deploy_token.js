ef25080ea43a169ffe8b2652d7484a4a6a213e6e
Sergei Stadnik <sergeisqa@gmail.com>
2019-03-26 10:14:43 -0700

    Migration success!

---
+++ migrations/2_deploy_token.js
@@ -0,0 +1,10 @@
+const MerxNetToken = artifacts.require("./MerxNetToken.sol");
+
+module.exports = function(deployer) {
+const _name = 'MerxNetToken';
+const _symbol = 'MERX';
+const _decimals = 18;
+
+
+  deployer.deploy(MerxNetToken, _name, _symbol, _decimals );
+};
